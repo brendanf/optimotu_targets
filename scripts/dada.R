9ca8cfff48a355efecdec444d89d53524c5e1311
brendanf <brendan.furneaux@gmail.com>
2022-02-15 15:17:44 +0200

    fill out dada script

--- scripts/dada.R
+++ scripts/dada.R
@@ -6,15 +6,19 @@ library(dada2)
 library(tidyverse)
 library(magrittr)
 
+#### Get the samples organized ####
+
 #define paths
 path <- "/scratch/project_2003104/brendan/sonja_spruce_logs"
 seq_path <- file.path(path, "sequences")
 #raw_path <- file.path(seq_path, "01_raw")
 raw_path <- file.path(path, "raw_data")
 filt_path <- file.path(seq_path, "02_filter")
+asv_path <- file.path(seq_path, "03_denoised")
 
 # create paths if missing
 if (!dir.exists(filt_path)) dir.create(filt_path, recursive = TRUE)
+if (!dir.exists(asv_path)) dir.create(asv_path, recursive = TRUE)
 
 # find files
 sample_table <- tibble(
@@ -34,6 +38,7 @@ sample_table <- tibble(
     filt_R2 = file.path(filt_path, paste0(sample, "_R2_filt.fastq.gz"))
   )
 
+#### Quality filtering and trimming ####
 out2 <- sample_table %$%
   filterAndTrim(
     fwd = file.path(raw_path, fastq_R1),
@@ -43,8 +48,40 @@ out2 <- sample_table %$%
     maxN = 0, # max 0 ambiguous bases
     maxEE = c(3, 5), # max expected errors (fwd, rev)
     truncQ = 2, # truncate at first base with quality <= 2
-    rm.phix = TRUE, #remove matches to phix genome
+    rm.phix = TRUE, #remove matches to phiX genome
     minLen = 50, # remove reads < 50bp
     compress = TRUE, # write compressed files
-    multithread = FALSE 
+    multithread = TRUE 
   )
+
+#### Fit error model ####
+errR1 <- sample_table %$%
+  learnErrors(file.path(filt_path, filt_R1), multithread = TRUE)
+errR2 <- sample_table %$%
+  learnErrors(file.path(filt_path, filt_R2), multithread = TRUE)
+
+#### Dereplicate reads ####
+derepR1 <- sample_table %$%
+  derepFastq(file.path(filt_path, filt_R1), verbose = TRUE)
+names(derepR1) <- sample_table$sample
+
+derepR2 <- sample_table %$%
+  derepFastq(file.path(filt_path, filt_R2), verbose = TRUE)
+names(derepR2) <- sample_table$sample
+
+#### Denoise ####
+dadaR1 <- dada(derepR1, err = errR1, multithread = TRUE, verbose = TRUE)
+dadaR2 <- dada(derepR2, err = errR2, multithread = TRUE, verbose = TRUE)
+
+#### Merge paired reads ####
+merged <- mergePairs(dadaR1, derepR1, dadaR2, derepR2,
+                                minOverlap = 10, maxMismatch = 1, verbose=TRUE)
+
+#### Merge no-mismatch pairs ####
+
+
+
+#### Generate ASV table ####
+asvtab <- makeSequenceTable(mergers)
+saveRDS(file.path(asv_path, "asv_tab.rds"))
+save.image(file.path(asv_path, "denoising.Rdata"))

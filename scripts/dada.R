3e0d755925a09c0926ce10d016c3c28fbf680051
brendanf <brendan.furneaux@gmail.com>
2022-02-15 17:08:27 +0200

    parse sequencing metadata

--- scripts/dada.R
+++ scripts/dada.R
@@ -25,6 +25,7 @@ cat("Running DADA2 with", ncpus, "cores.\n" )
 
 #define paths
 path <- "."
+meta_path <- file.path(path, "metadata")
 seq_path <- file.path(path, "sequences")
 raw_path <- file.path(seq_path, "01_raw")
 filt_path <- file.path(seq_path, "02_filter")
@@ -33,6 +34,18 @@ asv_path <- file.path(seq_path, "03_denoised")
 # create paths if missing
 if (!dir.exists(filt_path)) dir.create(filt_path, recursive = TRUE)
 if (!dir.exists(asv_path)) dir.create(asv_path, recursive = TRUE)
+
+# Get sequencing metadata
+
+metadata_files <- list.files(path = meta_path, pattern = "*.xlsm", full.names = TRUE)
+metadata <- purrr::map_dfr(
+  metadata_files,
+  readxl::read_xlsx,
+  skip = 2,
+  col_types = c("text", "text", rep("skip", 14))
+) %>%
+  tidyr::separate(col = "Sample Locator", into = c("seqrun", "well"), sep = " ") %>%
+  dplyr::filter(!is.na(`BOLD Sample IDs`))
 
 # find files
 sample_table <- tibble(
@@ -45,7 +58,7 @@ sample_table <- tibble(
     into = "sample",
     regex = paste0(
       "(?:MI_M06648_\\d+\\.001\\.N\\d+--S\\d+\\.)?",
-      "(19[KFLS][EAU]\\d{3}|CCDB-\\d+NEG(?:EXT|PCR[12]))",
+      "(19[KFLS][EAU]\\d{3}|CCDB-\\d+NEG(?:EXT|PCR[12])|BLANK\\d)",
       "_(?:S\\d+_L001_)?R1(?:_001)?.fastq.gz"
     ),
     remove = FALSE
@@ -55,7 +68,9 @@ sample_table <- tibble(
     filt_R1 = file.path(filt_path, paste0(sample, "_R1_filt.fastq.gz")),
     filt_R2 = file.path(filt_path, paste0(sample, "_R2_filt.fastq.gz"))
   ) %>%
-  dplyr::filter(!is.na(sample))
+  # dplyr::filter(!is.na(sample)) %>%
+  dplyr::left_join(metadata, by = c("sample" = "BOLD Sample IDs")) %>%
+  dplyr::mutate(seqrun = dplyr::coalesce(seqrun, substring(sample, 1, 10)))
 
 cat("Found", nrow(sample_table), "samples.\n")
 

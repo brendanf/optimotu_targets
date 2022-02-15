bd53fa36d035b4c88b7f3c9864e39c2edad833b6
brendanf <brendan.furneaux@gmail.com>
2022-02-15 17:09:17 +0200

    add skeleton of targets plan

---
+++ _targets.R
@@ -0,0 +1,26 @@
+# Bioinformatics pipeline for Sonja Saine's spruce log metabarcoding project
+#   by Brendan Furneaux
+# Based on:
+#   DADA2 analysis for GSSP from Jenni Hultman
+#   PROTAX taxonomy assignment for GSSP by Panu Somervuo
+#   Clustering for GSSP by Brendan Furneaux
+
+# ensure we have all necessary packages
+# if already installed (e.g. on CSC) this will copy them once per user to a
+# cache in ~/.cache/R
+renv::hydrate()
+
+library(targets)
+library(tarchetypes)
+library(magrittr)
+
+# Numbered R scripts define the targets plan.
+# They are numbered in the order they are used.
+for (f in list.files("scripts", "\\d{3}_.+.R", full.names = TRUE)) {
+  source(f)
+}
+
+# End this file with a list of target objects.
+list(
+  
+)

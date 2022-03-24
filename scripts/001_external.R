8bddf1de96eedffb3f5e5f9bdc6d12b0caefce2a
brendanf <brendan.furneaux@gmail.com>
2022-03-24 09:41:13 +0200

    copy vsearch_cluster_smallmem() from GSSP

---
+++ scripts/001_external.R
@@ -0,0 +1,28 @@
+#### Functions which call external software from R
+# Brendan Furneaux 2022
+
+vsearch_cluster_smallmem <- function(seq, threshold = 1, ncpu = local_cpus()) {
+  tout <- tempfile("data", fileext = "fasta")
+  tin <- tempfile("data", fileext = "uc")
+  on.exit(unlink(tout, force = TRUE))
+  write_sequence(seq, tout)
+  uc = system(
+    paste(
+      "vsearch",
+      "--cluster_smallmem", tout,
+      "--usersort",
+      "--id", threshold,
+      "--uc -",
+      "--threads", ncpu,
+      "| awk '$1==\"H\" {print $9,$10}'"
+    ),
+    intern = TRUE
+  )
+  stopifnot(attr(uc, "status") == 0)
+  readr::read_delim(
+    I(uc),
+    col_names = c("query", "hit"),
+    delim = " ",
+    col_types = "cc"
+  )
+}

1de104f65d8cf1da7b69b6a54bf9226f2cfd0138
Brendan Furneaux <brendan.furneaux@gmail.com>
2022-03-25 19:35:24 +0100

    fix/improve cluster_smallmem

--- scripts/001_external.R
+++ scripts/001_external.R
@@ -2,8 +2,8 @@
 # Brendan Furneaux 2022
 
 vsearch_cluster_smallmem <- function(seq, threshold = 1, ncpu = local_cpus()) {
-  tout <- tempfile("data", fileext = "fasta")
-  tin <- tempfile("data", fileext = "uc")
+  tout <- tempfile("data", fileext = ".fasta")
+  tin <- tempfile("data", fileext = ".uc")
   on.exit(unlink(tout, force = TRUE))
   write_sequence(seq, tout)
   uc = system(
@@ -19,10 +19,16 @@ vsearch_cluster_smallmem <- function(seq, threshold = 1, ncpu = local_cpus()) {
     intern = TRUE
   )
   stopifnot(attr(uc, "status") == 0)
-  readr::read_delim(
-    I(uc),
-    col_names = c("query", "hit"),
-    delim = " ",
-    col_types = "cc"
-  )
+  if (length(uc) > 0) {
+    readr::read_delim(
+      I(uc),
+      col_names = c("query", "hit"),
+      delim = " ",
+      col_types = "cc"
+    )
+  } else {
+    tibble::tibble(query = character(), hit = character())
+  }
+}  stopifnot(out == 0)
+  c(trim_R1, trim_R2)
 }

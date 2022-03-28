e71072785be99ab2cb0a736e5877963071fa4694
brendanf <brendan.furneaux@gmail.com>
2022-03-28 13:18:59 +0300

    updates to slurm script; now run in custom singularity container

--- run_node.sh
+++ run_node.sh
@@ -2,15 +2,15 @@
 
 #filter and assign taxonomy to demultiplexed Illumina reads
 
-#SBATCH --job-name spruce-log-fungi
+#SBATCH --job-name deadwood_restoration
 #SBATCH --account project_2003104
 #SBATCH --partition small
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 8
 #SBATCH --nodes 1
-#SBATCH --mem-per-cpu 8G
-#SBATCH --time 1:00:00
+#SBATCH --mem 256G
+#SBATCH --time 24:00:00
 #SBATCH --mail-type ALL
 
-module load r-env-singularity/4.0.5
-srun singularity_wrapper exec Rscript scripts/dada.R
+export PATH=$(pwd)/conda/deadwood_restoration/bin:$PATH
+srun R --no-save -e 'targets::tar_make(callr_function=NULL)'

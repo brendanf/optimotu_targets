28713c4f395b0b552993a89b7c75feda2098bb37
brendanf <brendan.furneaux@gmail.com>
2022-04-01 09:17:04 +0300

    add function to calculate fmeasure

---
+++ scripts/002_functions.R
@@ -0,0 +1,26 @@
+# faster than length(intersect(x, y)), but assumes x = unique(x) and y = unique(y)
+# this could be even faster since we know they are sorted, but there is no fast R
+# function I am aware of.
+intersect_length <- function(x, y) sum(!is.na(match(x, y)))
+
+inner_fmeasure <- function(cj, kpartition, nk) {
+  nc <- length(cj)
+  nc * max(purrr::map_int(kpartition, intersect_length, cj)/(nc + nk))
+}
+
+# Calculate F-measure for delimitation by clustering
+f_measure <- function(data, c, k) {
+  nseq <- nrow(data)
+  cpartition <- split(seq_along(data[[c]]), data[[c]])
+  kpartition <- split(seq_along(data[[k]]), data[[k]])
+  nk <- purrr::map_int(kpartition, length)
+  2 / nseq * sum(
+    purrr::map_dbl(
+      cpartition,
+      inner_fmeasure,
+      kpartition,
+      nk
+    )
+  )
+}
+

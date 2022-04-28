4ee0c039e52f729f44654bb37b942017e25d2669
brendanf <brendan.furneaux@gmail.com>
2022-04-28 20:07:33 +0300

    give sh_matching links to executables from conda

---
+++ scripts/012_link_executables.R
@@ -0,0 +1,18 @@
+# put links to programs in the place where sh_matching will look for them
+# (./bin will be bound to /sh_matching/programs)
+
+link_executable <- function(filename) {
+  unlink(filename)
+  ensure_directory(filename)
+  file.symlink(Sys.which(basename(filename)), filename)
+  filename
+}
+
+withr::with_dir(
+  "bin",
+  {
+    link_executable("ITSx/ITSx")
+    link_executable("blast-2.2.26/bin/blastclust")
+    link_executable("krona/bin/ktImportText")
+  }
+)

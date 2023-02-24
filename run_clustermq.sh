18904dd19db3202a0302ebd5694ac74043b1879f
brendanf <brendan.furneaux@gmail.com>
2023-02-24 16:09:04 +0200

    remove code to spoof sh_matching container

--- run_clustermq.sh
+++ run_clustermq.sh
@@ -12,10 +12,5 @@
 #SBATCH --time 48:00:00
 #SBATCH --mail-type ALL
 
-SINGULARITY_BIND="bin/usearch11.0.667_i86linux32:/sh_matching/programs/usearch"
-SINGULARITY_BIND="${SINGULARITY_BIND},sh_matching_pub/sh_matching_analysis/scripts:/sh_matching/scripts"
-SINGULARITY_BIND="${SINGULARITY_BIND},sh_matching_pub/sh_matching_analysis/readme.txt:/sh_matching/readme.txt" 
-SINGULARITY_BIND="${SINGULARITY_BIND},data/sh_matching_data:/sh_matching/data"
-export SINGULARITY_BIND
 export PATH="/projappl/project_2003156/its2_taxonomy_first/bin:$PATH"
 R --vanilla -f run_clustermq.R

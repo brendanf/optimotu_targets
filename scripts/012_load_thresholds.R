dc915ac950e57cc819880b4915e2bb83b3be17e2
brendanf <brendan.furneaux@gmail.com>
2023-10-23 20:38:44 +0300

    remove: threshold optimization (use GSSP optima)

--- scripts/012_load_thresholds.R
+++ scripts/012_load_thresholds.R
@@ -1,5 +1,6 @@
 # test the performance of different clustering thresholds for replicating
 # "true" taxonomic groups.
 
-# threshold_file <- file.path(meta_path, "GSSP_thresholds.tsv")
-# fmeasure_optima <- readr::read_tsv(threshold_file, col_types = "ccccdd")
+threshold_file <- file.path(meta_path, "GSSP_thresholds.tsv")
+fmeasure_optima <- readr::read_tsv(threshold_file, col_types = "ccccdd") |>
+  dplyr::filter(metric == "mFM")

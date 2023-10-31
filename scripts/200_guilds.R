ea315f3dad010f4aedbb48dfd5b4fb2d68c74916
brendanf <brendan.furneaux@gmail.com>
2023-10-31 18:47:28 +0200

    add: assign guilds
    (cherry picked from commit 6471f3ca515cf6071667dffdf8e53d0f650bef0d)
    
    fix: add missing dependency on FUNGuildR
    
    (cherry picked from commit 77e290cb6da5d39dfebe6a4290067aa69dfb813b)
    
    fix: update to correct version of FUNGuildR (available for R 4.2)
    
    (cherry picked from commit 95ec7780222d7b0851c30df828e283c24078d0be)

---
+++ scripts/200_guilds.R
@@ -0,0 +1,44 @@
+guild_plan <- list(
+  #### funguild_db ####
+  tar_fst_tbl(
+    funguild_db,
+    FUNGuildR::get_funguild_db()
+  ),
+
+  #### map over confidence levels ####
+  tar_map(
+    # also map over some previously mapped targets
+    values = tibble::tibble(
+      .conf_level = c("plausible", "reliable"),
+      otu_taxonomy = paste0("otu_taxonomy_", .conf_level) %>%
+        rlang::syms()
+    ),
+    names = .conf_level,
+
+    ##### otu_guild_{.conf_level} #####
+    tar_fst_tbl(
+      otu_guild,
+      otu_taxonomy |>
+        dplyr::mutate(
+          dplyr::across(
+            genus:species,
+            sub,
+            pattern = "([A-Z].+)_[0-9]+",
+            replacement = "\\1"
+          )
+        ) |>
+        tidyr::unite("Taxonomy", kingdom:species, sep = ",") |>
+        FUNGuildR::funguild_assign(db = funguild_db) |>
+        dplyr::select(OTU, guild)
+    ),
+    ##### write_otu_guild_{.conf_level} #####
+    tar_file(
+      write_otu_guild,
+      write_and_return_file(
+        otu_guild,
+        sprintf("output/otu_guilds_%s.tsv", .conf_level),
+        type = "tsv"
+      )
+    )
+  )
+)

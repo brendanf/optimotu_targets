fbbd4094d669690255f575ca188bc4b174376a93
brendanf <brendan.furneaux@gmail.com>
2024-12-04 15:02:23 +0200

    update sample list

--- metadata/create_sample_list.R
+++ metadata/create_sample_list.R
@@ -6,10 +6,10 @@ raw_path <- file.path(seq_path, "01_raw")
 trim_path <- file.path(seq_path, "02_trim")
 filt_path <- file.path(seq_path, "03_filter")
 
-cyclone_sample_regex <- "^C[0-9A-Z]{5}$"
-cyclone_neg_regex <- "^((GSSP|CCDB)-\\d{5})?(NEGEXT|NEGPCR[12]|control[123])$"
+cyclone_sample_regex <- "^(C[0-9A-Z]{5}$|[A-Z]{3}-(\\d{2}-)?([Ww]eek|extra))"
+cyclone_neg_regex <- "^((GSSP|CCDB)-\\d{5})?(NEGEXT[13]?|NEGPCR[12]|[Cc]ontrol\\d?|CONTROL\\d?|BLANK\\d?)$"
 soil_sample_regex <- "^LIFEP-GSSP-([12])-(S[A-Z0-9]{5})$"
-soil_neg_regex <- "^LIFEP-GSSP-([12])-((Neg|PCR)\\d{1,2})$"
+soil_neg_regex <- "^LIFEP-GSSP-([12])-((Neg|PCR)\\d{1,2}|NEGBSA|PCRBSA)$"
 
 sample_table <- tibble::tibble(
     fastq_R1 = sort(list.files(raw_path, ".*R1(_001)?.fastq.gz", recursive = TRUE)),
@@ -24,6 +24,12 @@ sample_table <- tibble::tibble(
   ) |>
   dplyr::mutate(
     seqrun = if (dplyr::n_distinct(lane) > 1L) paste(seqrun, lane, sep = "_") else seqrun,
+    sample = dplyr::case_match(
+      sample,
+      "3ACBN-22-week-" ~ "CBN-22-week-3A",
+      "ZUR-2-week-41A" ~ "ZUR-21-week-41A",
+      .default = sample
+    ),
     .by = seqrun,
     .keep = "unused"
   ) |>
@@ -41,7 +47,7 @@ sample_table <- tibble::tibble(
       grepl(soil_sample_regex, sample) ~ sub(soil_sample_regex, "\\2_Rep\\1", sample),
       grepl(soil_neg_regex, sample) ~ paste(seqrun, sub(soil_neg_regex, "\\2_Rep\\1", sample), sep = "_")
     ),
-    cut_R2 = ifelse(startsWith(seqrun, "BIONAME"), "0", "16")
+    cut_R2 = ifelse(startsWith(seqrun, "CCDB"), "16", "0")
   ) |>
   dplyr::select(seqrun, sample, neg_control, cut_R2, fastq_R1, fastq_R2)
 

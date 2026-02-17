06c1e799dccf1a692fa4fe601d6aa22f2b1ba95b
brendanf <brendan.furneaux@gmail.com>
2026-02-17 14:27:00 +0200

    add stringfish to post-installation for tykky

--- conda/OptimOTU_v6_postinstall.sh
+++ conda/OptimOTU_v6_postinstall.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-R -e 'renv::install("bioconductor::dada2", "brendanf/optimotu@v0.9.5", "brendanf/optimotu.pipeline@v0.6.1")'
+R -e 'renv::install("bioconductor::dada2", "stringfish", "brendanf/optimotu@v0.9.5", "brendanf/optimotu.pipeline@v0.6.1")'
